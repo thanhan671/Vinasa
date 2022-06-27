9e2ec299f94df146b9b921c6a6a827db15068c6e
theanhbr01 <theanhbr01@gmail.com>
2022-06-28 01:41:55 +0700

    Up

---
+++ Vinasa/DAL/SeminarContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Vinasa.Models;
+
+namespace Vinasa.DAL
+{
+    public class SeminarContext : DbContext 
+    {
+        public SeminarContext() : base("SchoolContext")
+        {
+
+        }
+
+        public DbSet<Seminar> Seminars { get; set; }
+        public DbSet<SeminarParticipant> SeminarParticipants { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Vinasa/DAL/SeminarInitializer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using Vinasa.Models;
+
+namespace Vinasa.DAL
+{
+    public class SeminarInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SeminarContext>
+    {
+        protected override void Seed(SeminarContext context)
+        {
+            SeminarInitialize(context);
+            SeminarParticipantInitialize(context);
+            ProviceInitialize(context);
+        }
+
+        private static void SeminarInitialize(SeminarContext context)
+        {
+
+            // Look for any movies.
+            if (context.Seminars.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Seminars.AddOrUpdate(
+                new Seminar
+                {
+                    Title = "Hội nghị kết nối đầu tư tổ chức tại TP. Hồ Chí Minh: chủ đề THÚC ĐẨY ĐẦU TƯ VÀ PHÁT TRIỂN HỆ SINH THÁI TÀI CHÍNH SỐ",
+                    OpenDate = "9:00 - 17:00 ngày 28/06/2022 (Thứ 3)",
+                    Address = "Sihub, 273 Điện Biên Phủ, Phường Võ Thị Sáu, Quận 3, TP. HCM",
+                    CreatedUtc = DateTime.UtcNow
+                }
+            );
+            context.SaveChanges();
+        }
+        private static void SeminarParticipantInitialize(SeminarContext context)
+        {
+            if (context.SeminarParticipants.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.SeminarParticipants.AddOrUpdate(
+                new SeminarParticipant
+                {
+                    TaxNumber = "0315159393",
+                    SeminarId = 1,
+                    Company = "Công ty Cổ phần FPT",
+                    Name = "Trương Gia Bình",
+                    Position = "Chủ tịch HĐQT",
+                    Email = "manhnt@vinasa.org.vn",
+                    PhoneNumber = "0937688958",
+                    ProvinceId = 1,
+                    JobTitle = "Doanh nghiệp CNTT",
+                    Operation = "Công nghệ thông tin",
+                    RegistrySeminar = true,
+                    RegistryBusinessMatching = true,
+                    RegistryExhibition = true,
+                    RegistryTicket = true,
+                    CreatedUtc = DateTime.UtcNow
+                }
+            );
+            context.SaveChanges();
+        }
+
+        private static void ProviceInitialize(SeminarContext context)
+        {
+            if (context.SeminarParticipants.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Provinces.AddOrUpdate(
+                new Province
+                {
+                    Title = "HCM"
+                }
+            );
+            context.Provinces.AddOrUpdate(
+                new Province
+                {
+                    Title = "HN"
+                }
+            );
+            context.Provinces.AddOrUpdate(
+                new Province
+                {
+                    Title = "DN"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
