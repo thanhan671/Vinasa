d4b8e8bb3b80ce49774150d62d3d699b42e3d490
Khanh <57382524+COCC390@users.noreply.github.com>
2022-06-27 13:57:06 +0700

    add db to project

--- Vinasa/Controllers/AdminController.cs
+++ Vinasa/Controllers/AdminController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vinasa.Models;
 
 namespace Vinasa.Controllers
 {
     public class AdminController : Controller
     {
+        SEP25Team16Entities1 db = new SEP25Team16Entities1();
         // GET: Admin
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            
+            return View(db.TAIKHOANADMINs.ToList());
         }
         public ActionResult Edit()
         {
