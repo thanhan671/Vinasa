fd789c8e53a5ca4d696721dcb0bd9eeaa025d604
thanhan671 <thanhannguyen67@gmail.com>
2022-07-02 10:09:57 +0700

    done project

--- Vinasa/Controllers/AccountController.cs
+++ Vinasa/Controllers/AccountController.cs
@@ -39,14 +39,14 @@ namespace Vinasa.Controllers
                     }
                     else if (checkAccount != null && !checkAccount.TrangThai.Equals(1))
                     {
-                        ViewBag.Message = checkAccount.Email + " this account not activated, please contact the administrator to active this";
+                        ViewBag.Message = checkAccount.Email + " tài khoản này chưa được kích hoạt, vui lòng liên hệ với quản trị viên để kích hoạt tài khoản này";
                         return View();
                     }
 
                 }
             }
 
-            ViewBag.Message = "wrong email or password";
+            ViewBag.Message = "Tài khoản hoặc mật khẩu không đúng";
             return View();
         }
 
@@ -56,63 +56,6 @@ namespace Vinasa.Controllers
             Session["AccountID"] = null;
             Session.Abandon();
             return RedirectToAction("Login", "Account", new { area = " " });
-        }
-
-
-
-        [HttpGet]
-        public ActionResult Register()
-        {
-            AdminAccountModels accountModels = new AdminAccountModels();
-            return View(accountModels);
-        }
-
-        [HttpPost]
-        public ActionResult Register(AdminAccountModels adminAccountModels)
-        {
-            var registerEmail = adminAccountModels.Email.Trim();
-            var registerPassword = adminAccountModels.MatKhau.Trim();
-            var registerRePassword = adminAccountModels.reMatKhau.Trim();
-
-            if (registerPassword.Equals(registerRePassword))
-            {
-                using (db)
-                {
-                    var checkAccount = db.TAIKHOANADMINs.Where(acc => acc.Email.Equals(registerEmail.Trim())).FirstOrDefault();
-                    {
-                        if (checkAccount != null)
-                        {
-                            ViewBag.Message = registerEmail + " has been existing, please change another email or contact to the admin";
-                            return View();
-                        }
-                        else
-                        {
-                            try
-                            {
-                                TAIKHOANADMIN newAccount = new TAIKHOANADMIN();
-                                newAccount.Ten = adminAccountModels.Ten.Trim();
-                                newAccount.Email = registerEmail;
-                                newAccount.Sdt = adminAccountModels.Sdt.Trim();
-                                newAccount.PhongBan = adminAccountModels.PhongBan.Trim();
-                                newAccount.MatKhau = registerPassword;
-                                newAccount.Quyen = 2;
-                                newAccount.TrangThai = 2;
-
-                                db.TAIKHOANADMINs.Add(newAccount);
-                                db.SaveChanges();
-                                ViewBag.Message = newAccount.Ten + " create successfully, please wait for admin to activate this account";
-                                //return RedirectToAction("Login");
-                                return View();
-                            }
-                            catch (Exception ex)
-                            {
-                                throw ex;
-                            }
-                        }
-                    }
-                }
-            }
-            return View();
         }
 
         [HttpGet]
