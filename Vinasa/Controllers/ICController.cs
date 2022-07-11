960275f8b305ca57ac2e58fed8bc1b9630473b85
thanggiang <thang.giang113vn@gmail.com>
2022-07-11 12:01:31 +0700

    IC FE

---
+++ Vinasa/Controllers/ICController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vinasa.Controllers
+{
+    public class ICController : Controller
+    {
+        // GET: IC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult DeleteSelected()
+        {
+            return View();
+        }
+        public ActionResult CreateOrUpdate()
+        {
+            return View();
+        }
+    }
+}
---
+++ Vinasa/Controllers/ICParticipantsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vinasa.Controllers
+{
+    public class ICParticipantsController : Controller
+    {
+        // GET: ICParticipants
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
