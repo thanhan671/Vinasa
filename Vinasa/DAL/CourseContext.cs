06eec8300381f6c928a543401e3c214715239541
Khanh <57382524+COCC390@users.noreply.github.com>
2022-07-14 23:20:51 +0700

    small update

---
+++ Vinasa/DAL/CourseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Vinasa.Models;
+
+namespace Vinasa.DAL
+{
+    public class CourseContext : DbContext
+    {
+        public CourseContext() : base("CourseContext")
+        {}
+
+        //public DbSet<KHOAHOC> KHOAHOCs { get; set; }
+        //public DbSet<SeminarParticipant> SeminarParticipants { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //modelBuilder.Entity<KHOAHOC>().ToTable("KHOAHOCs");
+        }
+    }
+}
