6c41ede8cbb86b8598c44a14042912adcc0b7f59
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2022-07-18 23:16:10 +0700

    update import excel

--- Vinasa/Controllers/CourseParticipantsController.cs
+++ Vinasa/Controllers/CourseParticipantsController.cs
@@ -5,11 +5,12 @@ using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Vinasa.Services;
 using Vinasa.Models;
+using OfficeOpenXml;
+using System.Data.Entity.Validation;
 
 
 namespace Vinasa.Controllers
@@ -89,6 +90,64 @@ namespace Vinasa.Controllers
             var model = _db.THAMGIAKHOAHOCs.Where(m => m.Id == id).FirstOrDefault();
             ViewBag.IdKhoahoc = courseId;
             return PartialView("_DeleteSelected", model);
+        }
+
+        public ActionResult ImportExcel(FormCollection formCollection)
+        {
+            var courseParticipantsList = new List<THAMGIAKHOAHOC>();
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+                    using (var package = new ExcelPackage(file.InputStream))
+                    {
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First();
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                            var participants = new THAMGIAKHOAHOC();
+                            participants.HoTen = workSheet.Cells[rowIterator, 2].Value.ToString();
+                            participants.CongTyToChucCoQuan = workSheet.Cells[rowIterator, 3].Value.ToString();
+                            participants.ChucDanh = workSheet.Cells[rowIterator, 4].Value.ToString();
+                            participants.Email = workSheet.Cells[rowIterator, 5].Value.ToString();
+                            participants.Sdt = workSheet.Cells[rowIterator, 6].Value.ToString();
+                            participants.SoLuongHocVien = Convert.ToInt32(workSheet.Cells[rowIterator, 7].Value);
+                            participants.HoiVienVinasa = Convert.ToBoolean(workSheet.Cells[rowIterator, 8].Value);
+                            courseParticipantsList.Add(participants);
+                        }
+                    }
+                }
+            }
+            using (_db)
+            {
+                try
+                {
+                    foreach (var item in courseParticipantsList)
+                    {
+                        _db.THAMGIAKHOAHOCs.Add(item);
+                    }
+                    _db.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        throw new HttpException(eve.Entry.Entity.GetType().Name);
+                    }
+                    throw new HttpException(e.ToString());
+                }
+            }
+
+            return RedirectToAction("Index", "CourseParticipants", new { area = " " });
         }
 
     }
