1da8db1788ae240bc384f62623ac4b86d97312de
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2022-07-21 11:39:05 +0700

    update validation

--- Vinasa/Models/KHOAHOC.cs
+++ Vinasa/Models/KHOAHOC.cs
@@ -28,7 +28,8 @@ namespace Vinasa.Models
         public System.DateTime NgayBatDau { get; set; }
 
         [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [DateTimeRequired(ErrorMessage = "Thời gian diễn ra phải lớn hơn ngày hiện tại!")]
+        //[DateTimeRequired(ErrorMessage = "Thời gian diễn ra phải lớn hơn ngày bắt đầu!")]
+        [EndDateTimeRequired("NgayBatDau")]
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public System.DateTime NgayKetThuc { get; set; }
