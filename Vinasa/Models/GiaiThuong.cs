dfe9db65ce3d47e76101edd07362a5c2a29e8fb4
Thế Anh <theanhbr01@gmail.com>
2022-07-26 11:03:50 +0700

    up

--- Vinasa/Models/GiaiThuong.cs
+++ Vinasa/Models/GiaiThuong.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Vinasa.DAL;
 using Vinasa.Validation;
 
 namespace Vinasa.Models
 {
-    public class GIAITHUONG
+    public class GIAITHUONG: IValidatableObject
     {
         public GIAITHUONG()
         {
@@ -17,14 +18,12 @@ namespace Vinasa.Models
 
         [Display(Name = "Tên Giải Thưởng")]
         [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [GiaiThuongRequired(ErrorMessage = "Đã tồn tại hội nghị!")]
         [StringRequired(ErrorMessage = "Vui lòng điền trường này!")]
         public string Title { get; set; }
 
 
         [Display(Name = "Thời Gian Diễn Ra")]
         [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [DateTimeRequired(ErrorMessage = "Thời gian diễn ra phải lớn hơn ngày hiện tại!")]
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}")]
         public DateTime OpenDate { get; set; }
@@ -39,5 +38,22 @@ namespace Vinasa.Models
         public DateTime? CreatedUtc { get; set; }
 
         public List<NGUOINHANGIAITHUONG> NguoiNhanGiaiThuong { get; set; }
+
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+        {
+            SeminarContext _db = new SeminarContext();
+            List<ValidationResult> validationResult = new List<ValidationResult>();
+
+            if (_db.GIAITHUONG.Any(it => it.Title == Title && it.Id != Id))
+                validationResult.Add(new ValidationResult("Đã tồn tại giải thưởng!"));
+
+            var giaiThuong = _db.GIAITHUONG.FirstOrDefault(it => it.Id == Id);
+            if(giaiThuong == null)
+            {
+                if (OpenDate <= DateTime.Now)
+                    validationResult.Add(new ValidationResult("Thời gian kết thúc phải lớn hơn ngày hiện tại!"));
+            }
+            return validationResult;
+        }
     }
 }
