019a67144b621fe82d35c07bbcb84636f66b4785
thanhan671 <thanhannguyen67@gmail.com>
2022-07-28 16:31:54 +0700

    update sprint 2

--- Vinasa/Validation/CustomRequired.cs
+++ Vinasa/Validation/CustomRequired.cs
@@ -16,44 +16,6 @@ namespace Vinasa.Validation
             return (value != null && !string.IsNullOrEmpty(value.ToString()));
         }
     }
-
-    public class DateTimeRequired : ValidationAttribute
-    {
-        public override bool IsValid(object value)
-        {
-            if (value == null)
-                return false;
-
-            DateTime dateTime = Convert.ToDateTime(value);
-            return dateTime >= DateTime.Now;
-        }
-    }
-    public class EndDateTimeRequired : ValidationAttribute
-    {
-        private string startDate;
-        private const string _errorMessage = "Ngày kết thúc phải lớn hơn hoặc bằng ngày bắt đầu";
-
-        public EndDateTimeRequired(string dateToCompare) : base(_errorMessage)
-        {
-            startDate = dateToCompare;
-        }
-
-        public override string FormatErrorMessage(string name)
-        {
-            return string.Format(_errorMessage, name, startDate);
-        }
-
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            var dateToCompare = validationContext.ObjectType.GetProperty(startDate);
-            var dateToCompareValue = dateToCompare.GetValue(validationContext.ObjectInstance, null);
-            if (dateToCompareValue != null && value != null && (DateTime)value <= (DateTime)dateToCompareValue)
-            {
-                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
-            }
-            return null;
-        }
-    }
     public class DropDownListRequired : ValidationAttribute
     {
         public override bool IsValid(object value)
@@ -65,30 +27,5 @@ namespace Vinasa.Validation
             return iValue > 0;
         }
     }
-    public class CourseRequired : ValidationAttribute
-    {
-        private readonly SEP25Team16Entities2 _db = new SEP25Team16Entities2();
 
-        public override bool IsValid(object value)
-        {
-            if (value == null)
-                return false;
-
-            var course = _db.KHOAHOCs.FirstOrDefault(it => it.TenKhoaDaoTao == value.ToString());
-            return !(course != null && course.Id > 0);
-        }
-    }
-    public class ICRequired : ValidationAttribute
-    {
-        private readonly SEP25Team16Entities2 _db = new SEP25Team16Entities2();
-
-        public override bool IsValid(object value)
-        {
-            if (value == null)
-                return false;
-
-            var ic = _db.HOINGHIQUOCTEs.FirstOrDefault(it => it.Ten == value.ToString());
-            return !(ic != null && ic.ID > 0);
-        }
-    }
 }
