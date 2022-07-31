28d986f813b8ab4e7bab7c8c3ab658bc92ea3d1c
tienbap12 <tienbap12@gmail.com>
2022-07-31 14:21:12 +0700

    Statistics manage

---
+++ Vinasa/Controllers/StatisticController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vinasa.Controllers
+{
+    public class StatisticController : Controller
+    {
+        // GET: Statistic
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ActivityJoined()
+        {
+            return View();
+        }
+
+        public ActionResult MembershipFee()
+        {
+            return View();
+        }
+
+        public ActionResult BussinessByArea()
+        {
+            return View();
+        }
+
+        public ActionResult SizeOfPersonnel()
+        {
+            return View();
+        }
+
+        public ActionResult FieldOfActivity()
+        {
+            return View();
+        }
+
+        public ActionResult TimeOfEstablishment()
+        {
+            return View();
+        }
+
+        public ActionResult BirthDayInMonth()
+        {
+            return View();
+        }
+    }
+}
