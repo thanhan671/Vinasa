3c220e02d266c6e499c1a52f32ab054c46838fdc
thanggiang <thang.giang113vn@gmail.com>
2022-08-01 14:30:07 +0700

    18

---
+++ Vinasa/Controllers/OtherServiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vinasa.Controllers
+{
+    public class OtherServiceController : Controller
+    {
+        // GET: OtherService
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
