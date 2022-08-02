e7c7ae1f603a96fa446e2efabc29f58c02101ebb
thanhan671 <thanhannguyen67@gmail.com>
2022-08-02 10:16:54 +0700

    up FE sprint 3

---
+++ Vinasa/Controllers/OtherServicesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vinasa.Controllers
+{
+    public class OtherServicesController : Controller
+    {
+        // GET: OtherServices
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
