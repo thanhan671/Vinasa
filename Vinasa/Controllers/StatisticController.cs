ece4b2814e00271fe983bf4d3cb627bde1667b61
thanhan671 <thanhannguyen67@gmail.com>
2022-08-02 14:12:22 +0700

    update for sprint 3

---
+++ Vinasa/Controllers/StatisticController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vinasa.Controllers
+{
+    public class StatisticController : Controller
+    {
+        // GET: Statistic
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ActivityJoined()
+        {
+            return View();
+        }
+        public ActionResult BusinessByArea()
+        {
+            return View();
+        }
+        public ActionResult SizeOfPersonnel()
+        {
+            return View();
+        }
+        public ActionResult FieldOfActivity()
+        {
+            return View();
+        }
+        public ActionResult TimeOfEstablishment()
+        {
+            return View();
+        }
+        public ActionResult BirthDayInMonth()
+        {
+            return View();
+        }
+        public ActionResult MemberFee()
+        {
+            return View();
+        }
+    }
+}
