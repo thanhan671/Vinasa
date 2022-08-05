3cbf74740772973db7c160c70fbb5051a187036c
thanhan671 <thanhannguyen67@gmail.com>
2022-08-05 11:13:36 +0700

    done services

--- Vinasa/Controllers/ConnectionServicesController.cs
+++ Vinasa/Controllers/ConnectionServicesController.cs
@@ -1,25 +1,168 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Vinasa.Services;
+using Vinasa.Models;
+using OfficeOpenXml;
+using System.Data.Entity.Validation;
 
 namespace Vinasa.Controllers
 {
     public class ConnectionServicesController : Controller
     {
+        private readonly SEP25Team16Entities2 _db = new SEP25Team16Entities2();
+
         // GET: ConnectionServices
         public ActionResult Index()
         {
-            return View();
+            return View(_db.SUDUNGDICHVUKETNOIs.ToList());
         }
-        public ActionResult Details()
+        public ActionResult Details(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SUDUNGDICHVUKETNOI usingConnectionServices = _db.SUDUNGDICHVUKETNOIs.Find(id);
+            if (usingConnectionServices == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usingConnectionServices);
         }
-        public ActionResult Edit()
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SUDUNGDICHVUKETNOI usingConnectionServices = _db.SUDUNGDICHVUKETNOIs.Find(id);
+
+            if (usingConnectionServices == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(usingConnectionServices);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID, MaSoThue, TenCongTy, NgayBatDauHopDong, NgayKetThucHopDong, TenDichVu, GiaGoc, GiaUuDai, ChiecKhauVinasa, TenNguoiLienHe, ChucDanh, Email, DienThoai, GhiChu")] SUDUNGDICHVUKETNOI usingConnectionServices)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(usingConnectionServices).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usingConnectionServices);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var usingConnetionServices = _db.SUDUNGDICHVUKETNOIs.Where(t => t.ID.Equals(id)).FirstOrDefault();
+
+            _db.SUDUNGDICHVUKETNOIs.Remove(usingConnetionServices);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ImportExcel(FormCollection formCollection)
+        {
+            int addRow = 0;
+            int rowExist = 0;
+            var usingConnectionServicesList = new List<SUDUNGDICHVUKETNOI>();
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+
+                    using (var package = new ExcelPackage(file.InputStream))
+                    {
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First();
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                            string taxNumber = workSheet.Cells[rowIterator, 2].Value.ToString();
+                            string companyName = workSheet.Cells[rowIterator, 3].Value.ToString();
+                            DateTime endServiceDay = Convert.ToDateTime(workSheet.Cells[rowIterator, 5].Value);
+
+                            var usingConnectionServices = _db.SUDUNGDICHVUKETNOIs
+                                .FirstOrDefault(t => t.MaSoThue == taxNumber && t.TenCongTy == companyName && t.NgayKetThucHopDong == endServiceDay);
+                            if (usingConnectionServices == null)
+                            {
+                                var participants = new SUDUNGDICHVUKETNOI();
+                                participants.MaSoThue = workSheet.Cells[rowIterator, 2].Value.ToString();
+                                participants.TenCongTy = workSheet.Cells[rowIterator, 3].Value.ToString();
+                                participants.NgayBatDauHopDong = Convert.ToDateTime(workSheet.Cells[rowIterator, 4].Value);
+                                participants.NgayKetThucHopDong = Convert.ToDateTime(workSheet.Cells[rowIterator, 5].Value);
+                                participants.TenDichVu = workSheet.Cells[rowIterator, 6].Value.ToString();
+                                participants.GiaGoc = Convert.ToInt32(workSheet.Cells[rowIterator, 7].Value);
+                                participants.GiaUuDai = Convert.ToInt32(workSheet.Cells[rowIterator, 8].Value);
+                                participants.ChiecKhauVinasa = workSheet.Cells[rowIterator, 9].Value.ToString();
+                                participants.TenNguoiLienHe = workSheet.Cells[rowIterator, 10].Value.ToString();
+                                participants.ChucDanh = workSheet.Cells[rowIterator, 11].Value.ToString();
+                                participants.Email = workSheet.Cells[rowIterator, 12].Value.ToString();
+                                participants.DienThoai = workSheet.Cells[rowIterator, 13].Value.ToString();
+                                participants.GhiChu = Convert.ToString(workSheet.Cells[rowIterator, 14].Value);
+                                usingConnectionServicesList.Add(participants);
+                                addRow++;
+                            }
+                            else
+                            {
+                                rowExist++;
+                            }
+                        }
+                    }
+                }
+            }
+            using (_db)
+            {
+                try
+                {
+                    foreach (var item in usingConnectionServicesList)
+                    {
+                        _db.SUDUNGDICHVUKETNOIs.Add(item);
+                    }
+                    _db.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        throw new HttpException(eve.Entry.Entity.GetType().Name);
+                    }
+                    throw new HttpException(e.ToString());
+                }
+            }
+            Session["ViewBag.Success"] = addRow;
+            Session["ViewBag.Exist"] = rowExist;
+            return RedirectToAction("Index", "ConnectionServices");
+        }
+        public FileResult Download()
+        {
+            string path = Server.MapPath("~/Content/Files");
+            string filename = Path.GetFileName("MauDichVuKetNoi.xlsx");
+
+            string fullPath = Path.Combine(path, filename);
+            return File(fullPath, "download/xlsx", "MauDichVuKetNoi.xlsx");
         }
     }
 }
--- Vinasa/Controllers/OtherServicesController.cs
+++ Vinasa/Controllers/OtherServicesController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using OfficeOpenXml;
+using System.Data.Entity.Validation;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -48,38 +51,120 @@ namespace Vinasa.Controllers
         public ActionResult Edit([Bind(Include = "ID,MaSoThue,TenCongTy,NgayBatDauHopDong,NgayKetThucHopDong,TenDichVuKhac,GiaGoc," +
             "GiaUuDai,ChiecKhauVinasa,TenNguoiLienHe,ChucDanh,Email,DienThoai,GhiChu")] SUDUNGDICHVUKHAC otherParticipants)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(otherParticipants);
-            }
             if (ModelState.IsValid)
             {
                 _db.Entry(otherParticipants).State = System.Data.Entity.EntityState.Modified;
                 _db.SaveChanges();
-                return RedirectToAction("Details", "OtherServices");
+                return RedirectToAction("Index");
             }
+
             return View(otherParticipants);
         }
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, int icId = -1)
+        public ActionResult Delete(int id)
         {
-            var icParticipants = _db.THAMGIAHOINGHIQUOCTEs.Find(id);
-            icId = (int)icParticipants.HoiNghiQT_ID;
-            _db.THAMGIAHOINGHIQUOCTEs.Remove(icParticipants);
+            SUDUNGDICHVUKHAC dichVuKhac = _db.SUDUNGDICHVUKHACs.Find(id);
+            _db.SUDUNGDICHVUKHACs.Remove(dichVuKhac);
             _db.SaveChanges();
-            if (icId > 0)
-                return RedirectToAction("Details", "IC", new { id = icId });
-            else
-                return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index");
         }
 
-        public ActionResult DeleteSelected(int id, int icId = -1)
+        public ActionResult DeleteSelected(int id)
         {
-            var model = _db.THAMGIAHOINGHIQUOCTEs.Where(m => m.ID == id).FirstOrDefault();
-            ViewBag.IdHoiNghiQT = icId;
+            var model = _db.SUDUNGDICHVUKHACs.Where(m => m.ID == id).FirstOrDefault();
             return PartialView("_DeleteSelected", model);
+        }
+
+        public FileResult Download()
+        {
+            string path = Server.MapPath("~/Content/Files");
+            string filename = Path.GetFileName("MauDichVuKhac.xlsx");
+
+            string fullPath = Path.Combine(path, filename);
+            return File(fullPath, "download/xlsx", "MauDichVuKhac.xlsx");
+        }
+
+        public ActionResult ImportExcel(int? id, FormCollection formCollection)
+        {
+            int addRow = 0;
+            int rowExist = 0;
+            var usingOtherServicesList = new List<SUDUNGDICHVUKHAC>();
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+
+                    using (var package = new ExcelPackage(file.InputStream))
+                    {
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First();
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                            string taxNumber = workSheet.Cells[rowIterator, 2].Value.ToString();
+                            string companyName = workSheet.Cells[rowIterator, 3].Value.ToString();
+
+                            var maSoThue = _db.SUDUNGDICHVUKHACs
+                                .FirstOrDefault(t => t.MaSoThue == taxNumber && t.TenCongTy == companyName);
+                            if (maSoThue == null)
+                            {
+                                var participants = new SUDUNGDICHVUKHAC();
+                                participants.MaSoThue = workSheet.Cells[rowIterator, 2].Value.ToString();
+                                participants.TenCongTy = workSheet.Cells[rowIterator, 3].Value.ToString();
+                                participants.NgayBatDauHopDong = Convert.ToDateTime(workSheet.Cells[rowIterator, 4].Value);
+                                participants.NgayKetThucHopDong = Convert.ToDateTime(workSheet.Cells[rowIterator, 5].Value);
+                                participants.TenDichVuKhac = workSheet.Cells[rowIterator, 6].Value.ToString();
+                                participants.GiaGoc = Convert.ToInt32(workSheet.Cells[rowIterator, 7].Value);
+                                participants.GiaUuDai = Convert.ToInt32(workSheet.Cells[rowIterator, 8].Value);
+                                participants.ChiecKhauVinasa = workSheet.Cells[rowIterator, 9].Value.ToString();
+                                participants.TenNguoiLienHe = workSheet.Cells[rowIterator, 10].Value.ToString();
+                                participants.ChucDanh = workSheet.Cells[rowIterator, 11].Value.ToString();
+                                participants.Email = workSheet.Cells[rowIterator, 12].Value.ToString();
+                                participants.DienThoai = workSheet.Cells[rowIterator, 13].Value.ToString();
+                                participants.GhiChu = Convert.ToString(workSheet.Cells[rowIterator, 14].Value);
+                                usingOtherServicesList.Add(participants);
+                                addRow++;
+                            }
+                            else
+                            {
+                                rowExist++;
+                            }
+                        }
+                    }
+                }
+            }
+            using (_db)
+            {
+                try
+                {
+                    foreach (var item in usingOtherServicesList)
+                    {
+                        _db.SUDUNGDICHVUKHACs.Add(item);
+                    }
+                    _db.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        throw new HttpException(eve.Entry.Entity.GetType().Name);
+                    }
+                    throw new HttpException(e.ToString());
+                }
+            }
+            Session["ViewBag.Success"] = addRow;
+            Session["ViewBag.Exist"] = rowExist;
+            return RedirectToAction("Index", "OtherServices");
         }
     }
 }
