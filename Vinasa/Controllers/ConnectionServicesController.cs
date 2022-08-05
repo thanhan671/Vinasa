fc591d21ec1a67c19ab47783ef72b3d3464584c1
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2022-08-05 09:16:46 +0700

    add import function for connection services

--- Vinasa/Controllers/ConnectionServicesController.cs
+++ Vinasa/Controllers/ConnectionServicesController.cs
@@ -76,5 +76,87 @@ namespace Vinasa.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult ImportExcel(FormCollection formCollection)
+        {
+            int addRow = 0;
+            int rowExist = 0;
+            var usingConnectionServicesList = new List<SUDUNGDICHVUKETNOI>();
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+
+                    using (var package = new ExcelPackage(file.InputStream))
+                    {
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First();
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                            string taxNumber = workSheet.Cells[rowIterator, 2].Value.ToString();
+                            string companyName = workSheet.Cells[rowIterator, 3].Value.ToString();
+                            DateTime endServiceDay = Convert.ToDateTime(workSheet.Cells[rowIterator, 5].Value);
+
+                            var usingConnectionServices = _db.SUDUNGDICHVUKETNOIs
+                                .FirstOrDefault(t => t.MaSoThue == taxNumber && t.TenCongTy == companyName && t.NgayKetThucHopDong == endServiceDay);
+                            if (usingConnectionServices == null)
+                            {
+                                var participants = new SUDUNGDICHVUKETNOI();
+                                participants.MaSoThue = workSheet.Cells[rowIterator, 2].Value.ToString();
+                                participants.TenCongTy = workSheet.Cells[rowIterator, 3].Value.ToString();
+                                participants.NgayBatDauHopDong = Convert.ToDateTime(workSheet.Cells[rowIterator, 4].Value);
+                                participants.NgayKetThucHopDong = Convert.ToDateTime(workSheet.Cells[rowIterator, 5].Value);
+                                participants.TenDichVu = workSheet.Cells[rowIterator, 6].Value.ToString();
+                                participants.GiaGoc = Convert.ToInt32(workSheet.Cells[rowIterator, 7].Value);
+                                participants.GiaUuDai = Convert.ToInt32(workSheet.Cells[rowIterator, 8].Value);
+                                participants.ChiecKhauVinasa = workSheet.Cells[rowIterator, 9].Value.ToString();
+                                participants.TenNguoiLienHe = workSheet.Cells[rowIterator, 10].Value.ToString();
+                                participants.ChucDanh = workSheet.Cells[rowIterator, 11].Value.ToString();
+                                participants.Email = workSheet.Cells[rowIterator, 12].Value.ToString();
+                                participants.DienThoai = workSheet.Cells[rowIterator, 13].Value.ToString();
+                                participants.GhiChu = Convert.ToString(workSheet.Cells[rowIterator, 14].Value);
+                                usingConnectionServicesList.Add(participants);
+                                addRow++;
+                            }
+                            else
+                            {
+                                rowExist++;
+                            }
+                        }
+                    }
+                }
+            }
+            using (_db)
+            {
+                try
+                {
+                    foreach (var item in usingConnectionServicesList)
+                    {
+                        _db.SUDUNGDICHVUKETNOIs.Add(item);
+                    }
+                    _db.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        throw new HttpException(eve.Entry.Entity.GetType().Name);
+                    }
+                    throw new HttpException(e.ToString());
+                }
+            }
+            Session["ViewBag.Success"] = addRow;
+            Session["ViewBag.Exist"] = rowExist;
+            return RedirectToAction("Index", "ConnectionServices");
+        }
     }
 }
