1c608200599c60fbf802895eb6e22de97fb663ea
Thế Anh <theanhbr01@gmail.com>
2022-08-08 10:10:15 +0700

    Validate HoiPhi

--- Vinasa/Models/Statistic/HoiPhi.cs
+++ Vinasa/Models/Statistic/HoiPhi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,13 +13,25 @@ namespace Vinasa.Models
         public string TenCongTy { get; set; }   
         public string DiaChiGhiPhieuThu { get; set; }   
         public string DiaChiGuiPhieuThu { get; set; }   
-        public string NguoiNhanPhieu { get; set; }   
-        public int DienThoai { get; set; }   
-        public int HoiPhiNamTruoc { get; set; }   
-        public int HoiPhiNamSau { get; set; }   
-        public int TongThu { get; set; }   
-        public int DaDong { get; set; }   
-        public int ConLai { get; set; }   
+        public string NguoiNhanPhieu { get; set; }
+
+        public int DienThoai { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Giá trị phải lớn hơn hoặc bằng 0")]
+        public int HoiPhiNamTruoc { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Giá trị phải lớn hơn hoặc bằng 0")]
+        public int HoiPhiNamSau { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Giá trị phải lớn hơn hoặc bằng 0")]
+        public int TongThu { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Giá trị phải lớn hơn hoặc bằng 0")]
+        public int DaDong { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Giá trị phải lớn hơn hoặc bằng 0")]
+        public int ConLai { get; set; }  
+        
         public DateTime? NgayChuyenTien { get; set; }   
         public DateTime? NgayGuiPhieuThu { get; set; }   
         public string GhiChu { get; set; }   
