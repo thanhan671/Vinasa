341b830d68fb8964989569bd75e2813d2c6e8412
thanhan671 <thanhannguyen67@gmail.com>
2022-08-10 16:50:37 +0700

    update

--- Vinasa/Models/AdminAccountModels.cs
+++ Vinasa/Models/AdminAccountModels.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vinasa.Validation;
+using System;
 
 namespace Vinasa.Models
 {
@@ -14,26 +11,13 @@ namespace Vinasa.Models
         public int ID { get; set; }
 
         [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [StringRequired(ErrorMessage = "Vui lòng điền trường này!")]
         public string Ten { get; set; }
-
-        [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [StringRequired(ErrorMessage = "Vui lòng điền trường này!")]
-        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Không hợp lệ")]
         public string Email { get; set; }
         public int Quyen { get; set; }
         public int TrangThai { get; set; }
-
-        [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [StringRequired(ErrorMessage = "Vui lòng điền trường này!")]
-        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Không hợp lệ")]
         public string Sdt { get; set; }
-
-        [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [StringRequired(ErrorMessage = "Vui lòng điền trường này!")]
         public string PhongBan { get; set; }
 
-        [Required(ErrorMessage = "Vui lòng điền trường này!")]
         [MinLength(6, ErrorMessage = "Mật khẩu phải trên 6 ký tự")]
         public string MatKhau { get; set; }
 
@@ -44,7 +28,6 @@ namespace Vinasa.Models
         public string reMatKhau { get; set; }
         public string sQuyen { get; set; }
         public string sTrangThai { get; set; }
-        [Required(ErrorMessage = "Vui lòng điền trường này!")]
         public virtual QUYEN QUYEN1 { get; set; }
         public virtual TRANGTHAI TRANGTHAI1 { get; set; }
 
@@ -55,8 +38,6 @@ namespace Vinasa.Models
         [NotMapped]
         public SelectList StatusList { get; set; }
 
-        [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [StringRequired(ErrorMessage = "Vui lòng điền trường này!")]
         public string ChucDanh { get; set; }
     }
 }
